ba29ee857a33de0c9956ff0f614e6432ac66bc29
Strawhat76 <qutienpro@gmail.com>
2019-09-01 01:43:41 +0700

    Update database

---
+++ NapTheOnline/Models/ImageGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NapTheOnline.Models
+{
+    public partial class ImageGame
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DirPath { get; set; }
+        public int? GameId { get; set; }
+
+        public virtual Game Game { get; set; }
+    }
+}
--- NapTheOnline/Models/News.cs
+++ NapTheOnline/Models/News.cs
@@ -5,10 +5,17 @@ namespace NapTheOnline.Models
 {
     public partial class News
     {
+        public News()
+        {
+            ImageNews = new HashSet<ImageNews>();
+        }
+
         public int Id { get; set; }
         public string Logo { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string DateCreated { get; set; }
+
+        public virtual ICollection<ImageNews> ImageNews { get; set; }
     }
 }
