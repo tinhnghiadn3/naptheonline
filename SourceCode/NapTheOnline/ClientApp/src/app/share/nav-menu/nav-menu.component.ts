5701b4e8b31afd40a8b39d36a6800a8cf100cdd2
Ngoc Quynh Nguyen <tyquynhnguyen96@gmail.com>
2019-12-21 13:43:15 +0700

    Update banner and logo

--- SourceCode/NapTheOnline/ClientApp/src/app/share/nav-menu/nav-menu.component.ts
+++ SourceCode/NapTheOnline/ClientApp/src/app/share/nav-menu/nav-menu.component.ts
@@ -12,9 +12,10 @@ export class NavMenuComponent implements OnInit {
 
     MENU_ITEMS = MENU_ITEMS;
     searchExp: string;
+    selectedType: number = 0;
 
     constructor(private shareService: ShareService,
-                private router: Router) { }
+        private router: Router) { }
 
     ngOnInit() {
     }
@@ -22,7 +23,7 @@ export class NavMenuComponent implements OnInit {
     search() {
         if (this.searchExp && this.searchExp.trim().length > 0) {
             this.shareService.setSearchExp(this.searchExp);
-            if(this.router.url !== '/games') {
+            if (this.router.url !== '/games') {
                 this.router.navigate(['/games']);
             }
             // this.router.navigate(['/news']);
@@ -34,11 +35,15 @@ export class NavMenuComponent implements OnInit {
             this.shareService.setSearchExp(null);
         }
     }
-    
-    changeMenu(type: number) {
+
+    changeMenu(type: number, url: string) {
+        if (this.router.url === url && this.selectedType == type) {
+            return;
+        }
         this.shareService.setLoading(true);
         if (type && type > 0) {
             this.shareService.setNewType(type);
+            this.selectedType = type;
         }
     }
 }
